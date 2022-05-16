983ec4350ed135d182b74a25ff91b6a756ed30cf
SeanCranston <seancranston0@gmail.com>
2022-05-16 07:44:36 -0500

    adding new interval

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ library(caret)
 library(elasticnet)
 library(leaps)
 require(leaps)
+require(plsRglm)
 options(scipen = 999)
 
 
@@ -64,28 +65,96 @@ ui <- navbarPage(
                  ),
                  
                  mainPanel(
-                     uiOutput("Dat"),
+                     uiOutput("Dat_CI"),
                      #dataTableOutput("movieID"),
                      hr(),
                      #plotOutput("distPlot")
+                     uiOutput("Dat_PI")
                  )
              )
-    )
+    ),
     
-    # # Show a plot of the generated distribution
-    # mainPanel(
-    #     plotOutput("distPlot")
-    # )
+    navbarMenu("Behind the Scenes",
+               tabPanel("PLS Regression"),
+               tabPanel("Neural Network")
+             # fluidPage(theme = shinytheme("flatly")),
+             # # pageWithSidebar(
+             # #     headerPanel('Input Data'),
+             # #     
+             # #     # sidebarPanel(
+             # #     #     
+             # #     # ),
+             # #     # 
+             # #     # mainPanel(
+             # #     #     hr()
+             # #     # )
+             # # )
+             # 
+             # plotOutput(plot(mtcars$mpg,mtcars$disp))
+    )
 )
 
 
 #############################################################################
 server <- function(input, output) {
     
-    ##### this outputs the a row from movie_data that is to be rated
-    #values <- reactiveValues(dum = data.frame(x="test"))
-    # dummy data frame
-    output$Dat <- renderUI({
+
+    output$Dat_CI <- renderUI({
+        observed <- 
+            data.frame(
+                Target.Age = input$Target.Age,                      
+                Target.Employee.Count = input$Target.Employee.Count,
+                Net.Sales = input$Net.Sales,
+                Cost.of.Goods.Sold = input$Cost.of.Goods.Sold,                 
+                Rent = input$Rent,
+                Total.Operating.Expenses = input$Total.Operating.Expenses,                   
+                Interest.Expense = input$Interest.Expense,
+                Inventory.PPA = input$Inventory.PPA,  
+                Fixed.Assets.PPA = input$Fixed.Assets.PPA,        
+                Total.Intangibles.PPA = input$Total.Intangibles.PPA,  
+                Sales.Per.Square.Foot = input$Sales.Per.Square.Foot,
+                Gross.Profit.Margin.before.Operating.and.Net = input$Gross.Profit.Margin.before.Operating.and.Net,
+                Operating.Profit.Margin = input$Operating.Profit.Margin,
+                Net.Profit.Margin.after.Gross.and.Operating = input$Net.Profit.Margin.after.Gross.and.Operating ,                     
+                Sale.Date = input$Sale.Date
+            )
+        
+        # start predictions
+        pred <- list()
+        pred <- lapply(Models, predict, observed) %>% 
+            lapply(., round, digits = 2)
+        
+        #populate data frame with predictions an CI
+        x = data.frame(
+            Outcome = c("MVIC/EBIT", "MVIC/EBITDA", "MVIC", "MVIC/Net Sales"),
+            Estimate = c(pred$EBIT,
+                         pred$EBITDA,
+                         paste0("$",format(pred$Price, big.mark = ",")), 
+                         pred$Sales),
+            Lower_Estimate = c(
+                round(max(0,pred$EBIT-2*getTrainPerf(Models$EBIT)$TrainRMSE/sqrt(23)),digits = 2),
+                round(max(0,pred$EBITDA-2*getTrainPerf(Models$EBITDA)$TrainRMSE/sqrt(23)),digits = 2),
+                paste0("$",format(max(0,pred$Price-2*getTrainPerf(Models$Price)$TrainRMSE/sqrt(23)), big.mark = ",")),
+                round(max(0,pred$Sales-2*getTrainPerf(Models$Sales)$TrainRMSE/sqrt(23)),digits = 2)
+                
+            ),
+            Upper_Estimate = c(
+                pred$EBIT+2*getTrainPerf(Models$EBIT)$TrainRMSE/sqrt(23),
+                pred$EBITDA+2*getTrainPerf(Models$EBITDA)$TrainRMSE/sqrt(23),
+                paste0("$",format(pred$Price+2*getTrainPerf(Models$Price)$TrainRMSE/sqrt(23), big.mark = ",")),
+                pred$Sales+2*getTrainPerf(Models$Sales)$TrainRMSE/sqrt(23)
+                
+            ))
+        
+        x %>%
+            knitr::kable(format = "html") %>%
+            kable_styling("striped",full_width = T) %>%
+            add_header_above(c(" " = 2, "95% Confident Interval (Bounded by 0)" = 2)) %>%
+            HTML()
+        
+    })
+    
+    output$Dat_PI <- renderUI({
         observed <- 
             data.frame(
                 Target.Age = input$Target.Age,                      
@@ -135,7 +204,7 @@ server <- function(input, output) {
         x %>%
             knitr::kable(format = "html") %>%
             kable_styling("striped",full_width = T) %>%
-            add_header_above(c(" " = 2, "95% Confident Interval (Bounded by 0)" = 2)) %>%
+            add_header_above(c(" " = 2, "95% Prediction Interval (Bounded by 0)" = 2)) %>%
             HTML()
 
     })
